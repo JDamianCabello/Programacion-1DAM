5e8db9817cfb575eed4eb9d0a977026f52e82d98
Damián Cabello <jdamiancabello@gmail.com>
2017-12-04 09:13:32 +0100

    Añadimos código al ejemplo App_Strings.

--- Proyectos/App_Strings/App_Strings/Program.cs
+++ Proyectos/App_Strings/App_Strings/Program.cs
@@ -10,6 +10,23 @@ namespace App_Strings
     {
         static void Main(string[] args)
         {
+            string s1 = "Damián";
+            String s3 = new string('=',7); //Crea un array de n veces el char de la izquierda.
+            string s4 = s1 + s3;
+            string s2 = "Damián";
+
+            Console.WriteLine("Un texto".PadLeft(33));
+            Console.WriteLine(s1.PadLeft(33));
+            Console.WriteLine(s3.PadLeft(33));
+
+            Console.WriteLine(s1 == s2);
+            Console.WriteLine(s1.CompareTo(s2));
+            Console.WriteLine();
+
+            char[] sep = {' ', ','};
+            string[] palabras = s1.Split(sep, StringSplitOptions.RemoveEmptyEntries);
+
+            Console.ReadKey();
         }
     }
 }
