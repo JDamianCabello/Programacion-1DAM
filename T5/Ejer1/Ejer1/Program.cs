46905256803095da979a702defe031ef4dd9b426
Damián Cabello <jdamiancabello@gmail.com>
2017-12-08 19:26:51 +0100

    Añado El ejercicio 1 Terminado. Muestra el tipo y el máx/min de algunas variables.

---
+++ T5/Ejer1/Ejer1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Ejer1
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            char c = ' ';
+            int n = 0;
+            string[] dias = { "L", "M", "X", "J", "V", "S", "D" };
+
+            Console.WriteLine(" El tipo de un entero es: {0}", n.GetType());
+            Console.WriteLine(" El tipo char es: {0}", c.GetType());
+            Console.WriteLine(" El valor máximo de un entero es: {0}", int.MaxValue);
+            Console.WriteLine(" El valor máximo de un char es: {0}", (int)char.MaxValue);
+            Console.WriteLine(" El tamaño de dias es: {0}", dias.Length);
+
+            Console.ReadLine();
+        }
+    }
+}
