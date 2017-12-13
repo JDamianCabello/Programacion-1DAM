bb1e07dc7b547c5423c3122ff0123220e1c015f9
Damián Cabello <jdamiancabello@gmail.com>
2017-12-13 12:39:44 +0100

    Añado ejemplo en el que se añaden datos a un arraylist y luego se escriben en un foreach que lee objetos.

---
+++ Proyectos/App_Collection_ArrayList/App_Collection_ArrayList/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//----------------------------------------
+//Importante añadir los using´s.
+using System.Collections;
+
+namespace App_Collection_ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList lista = new ArrayList();
+            lista.Add("Damian");
+            lista.Add(125);
+
+            MostrarLista(lista);
+        }
+
+        static void MostrarLista(ArrayList lista)
+        {
+            foreach(Object tmp in lista)
+                Console.WriteLine(tmp);
+
+            Console.WriteLine("\n\nFin del listado....");
+            Console.ReadKey();
+        }
+    }
+}
