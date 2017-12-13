e9c745d196a099f57b9b5c8d72f4bb0766bfaa52
Damián Cabello <jdamiancabello@gmail.com>
2017-12-13 10:02:09 +0100

    Añado el ejercicio 1 Terminado (Escribir array al revés).

---
+++ T6/Ejer1/Ejer1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejer1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = string.Empty;
+            Console.WriteLine("Introduce una frase y la escibiré al revés:");
+            Console.Write("Frase: ");
+            frase = Console.ReadLine();
+            Console.Write("\nTu texto al reves: ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("{0}",AlReves(frase));
+            Console.ResetColor();
+            Console.WriteLine("\nPulsa una tecla para salir....");
+            Console.ReadKey();
+
+        }
+
+        static string AlReves(string s)
+        {
+            string aux = string.Empty;
+
+            for(int i = s.Length - 1; i >= 0; i--)
+                aux += s[i];
+            return aux;
+        }
+    }
+}
