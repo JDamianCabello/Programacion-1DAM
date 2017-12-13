e99f17c89fdfa0884a79424e6fea9a4a484327de
Damián Cabello <jdamiancabello@gmail.com>
2017-12-13 10:13:31 +0100

    Añado el ejercicio 2 Terminado (Quitar espacios por la derecha).

---
+++ T6/Ejer2/Ejer2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejer2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = string.Empty;
+            Console.WriteLine("Introduce una frase y quitaré los espacios del final:");
+            Console.Write("Frase: ");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            frase = Console.ReadLine();
+            Console.ResetColor();
+            Console.Write("\nTu texto sin espacios: ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("{0}", TrimD(frase));
+            Console.ResetColor();
+            Console.WriteLine("\nPulsa una tecla para salir....");
+            Console.ReadKey();
+        }
+
+        static string TrimD(string s)
+        {
+            int aux = s.Length - 1;
+            while(Char.IsSeparator(s[aux]))
+                aux--;
+            return s.Substring(0, aux + 1);
+            
+        }
+    }
+}
