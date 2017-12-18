7d58759a5a199c20887a64d2692441ea5e9438df
Damián Cabello <jdamiancabello@gmail.com>
2017-12-18 08:18:12 +0100

    Añado la struct carta para el ejemplo.

---
+++ Proyectos/App_Clase_List_Cartas/App_Clase_List_Cartas/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Clase_List_Cartas
+{
+    class Program
+    {
+        enum Palos { Oro, Copa, Espada, Basto };
+
+        struct Carta
+        {
+            #region CamposDeLaEstructura
+
+            int valor;      //Valor de la carta.
+            string nombre;  //Nombre de la carta
+            Palos palo;     //Palo de la carta.
+            double peso;    //Peso de carta dado el juego. 
+
+            #endregion
+
+
+            /// <summary>
+            /// Crea una structura del tipo carta.
+            /// </summary>
+            /// <param name="valor">El valor numérico de la carta.</param>
+            /// <param name="palo">El palo de la carta.</param>
+            public Carta(int valor, Palos palo)
+            {
+                string[] nombres = { "Uno", "Dos", "Tres", "Cuatro", "Cinco", "Seis", "Siete", "Ocho", "Nueve", "Sota", "Caballo", "Rey" };
+
+                valor = valor < 1 ? 1 : valor;
+                valor = valor > 12 ? 12 : valor;
+
+                this.valor = valor;
+                this.nombre = nombres[valor - 1];
+                this.palo = palo;
+
+                this.peso = valor >= 1 && valor <= 9 ? valor : .5;  //.x es lo mismo que poner 0.x
+            }
+
+
+
+            #region Método que acceden a los campos PRIVADOS.
+
+            public int GetValor() { return this.valor; }
+            public string GetNombre() { return this.nombre; }
+            public Palos GetPalo() { return this.palo; }
+            public double GetPeso() { return this.peso; }
+
+            public override string ToString()
+            {
+                string[] nombres = { "Uno", "Dos", "Tres", "Cuatro", "Cinco", "Seis", "Siete", "Ocho", "Nueve", "Sota", "Caballo", "Rey" };
+                return nombres[valor - 1] + " de " + palo.ToString();
+            }
+
+            #endregion
+        }
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
