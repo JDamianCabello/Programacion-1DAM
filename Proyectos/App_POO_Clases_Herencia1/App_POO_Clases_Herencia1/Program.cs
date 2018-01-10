898f077b175add5ab6a76978e3baeeef61e3f33f
Damián Cabello <jdamiancabello@gmail.com>
2018-01-10 12:15:15 +0100

    Continuamos la teoría de POO y añado ejemplos.

---
+++ Proyectos/App_POO_Clases_Herencia1/App_POO_Clases_Herencia1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_POO_Clases_Herencia1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClaseDerivada cd = new ClaseDerivada();
+            Console.WriteLine(cd.Saluda());
+        }
+    }
+
+    //Esta clase tiene como base Object.
+    abstract class ClaseBase
+    {
+        public string Saluda(){return "Hola primoh, soy la claseBase...";}
+        public abstract string SaludaAbs(string s);
+    }
+
+    //Esta clase hereda de ClaseBase y tiene por tanto el método Saluda().
+    class ClaseDerivada : ClaseBase
+    {
+        public override string SaludaAbs(string s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ T6/Ejer7/Ejer7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejer7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ValidarISBN("847-210-262-9"));
+            Console.WriteLine(ValidarISBN("8421728030124"));
+
+        }
+
+        static bool ValidarISBN(string isbn)
+        {
+            string auxISBN = string.Empty;
+            int suma = 0;
+            char prueba;
+            char control = isbn[isbn.Length - 1];
+
+            for(int i = 0; i < isbn.Length - 1; i++)
+                if(char.IsDigit(isbn[i]))
+                    auxISBN += isbn[i];
+
+
+
+            for(int i = 1; i < auxISBN.Length - 1; i++)
+                suma += (int)char.GetNumericValue(auxISBN[i - 1]) * i;
+
+            prueba = (suma %= 11) > 9 ? 'X' : (char)(suma %= 11);
+            Console.WriteLine(auxISBN);
+            Console.WriteLine("Prueba {0}", prueba);
+            Console.WriteLine("suma {0}", suma);
+            return prueba == control ? true : false;
+
+        }
+    }
+}
