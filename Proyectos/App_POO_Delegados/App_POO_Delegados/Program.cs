3f61dc720a01ea8d5dff5a87afc71fb93115a1b7
Damián Cabello <jdamiancabello@gmail.com>
2018-01-18 09:17:52 +0100

    Ejemplo y teoría de delegados.

---
+++ Proyectos/App_POO_Delegados/App_POO_Delegados/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_POO_Delegados
+{
+    class Program
+    {
+        //Declaro el tipo de dato delegado llamado "MiDelegado"
+        public delegate void MiDelegado(string msj);
+
+        static void Main(string[] args)
+        {
+            //Creo una instancia de tipo delegado "MiDelegado"
+            MiDelegado llamada = MetodoQueUsaElDelegado;
+
+            llamada("Hola soy el método a través de un delegado!.");
+            MetodoConDevolucion("La vivlia premoh", llamada);
+            Console.ReadLine();
+        }
+
+        static void MetodoQueUsaElDelegado(string mensaje)
+        {
+            Console.WriteLine(mensaje);
+        }
+
+        //Creamos un método que recibe como parámetro otro método.
+        static void MetodoConDevolucion(string texto, MiDelegado delegado)
+        {
+        }
+    }
+}
