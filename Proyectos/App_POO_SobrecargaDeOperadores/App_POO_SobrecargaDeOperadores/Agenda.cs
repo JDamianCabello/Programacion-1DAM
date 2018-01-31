2f00ff2e2ff6dd641bd16e35e5aa3401bfadf10d
Damián Cabello <jdamiancabello@gmail.com>
2018-01-31 11:49:45 +0100

    Añado ejemplo de sobrecarga de operadores.

---
+++ Proyectos/App_POO_SobrecargaDeOperadores/App_POO_SobrecargaDeOperadores/Agenda.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_POO_SobrecargaDeOperadores
+{
+    class Agenda
+    {
+        const int NUMMAXENTRADAS = 100;
+        string[] anotaciones = new string[NUMMAXENTRADAS];
+        int totalEntradas = 0;
+
+
+        public void ListarAnotaciones()
+        {
+            for(int i = 0; i < totalEntradas; i++)
+                Console.WriteLine("[{0}]: {1}",i + 1, anotaciones[i]);
+
+
+            Console.Write("\n\nNo hay más elementos en esta agenda... ");
+        }
+
+        //Definición del operador "+" para añadir anotaciones
+        public static bool operator +(Agenda unaAgenda, string anotacion)
+        {
+            if(unaAgenda.totalEntradas == NUMMAXENTRADAS)
+                return false;
+            unaAgenda.anotaciones[unaAgenda.totalEntradas++] = anotacion;
+            return true;
+        }
+    }
+}
---
+++ Proyectos/App_POO_SobrecargaDeOperadores/App_POO_SobrecargaDeOperadores/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_POO_SobrecargaDeOperadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Agenda AgendaSecreta = new Agenda();
+            bool seAnadio = false;
+
+
+            /*
+             * if(AgendaSecreta + "Añado el primer contacto")
+             *      Console.WriteLine("Contacto añadico con mucho esitoh");
+             */
+
+            for(int i = 0; i < 100; i++)
+                seAnadio = (AgendaSecreta + "Una Entrada");
+            
+            AgendaSecreta.ListarAnotaciones();
+
+            Console.ReadKey();
+        }
+    }
+}
