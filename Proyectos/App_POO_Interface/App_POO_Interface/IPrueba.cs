d149c977e68d0100cba4c63708ae27c5fec0f53d
Damián Cabello <jdamiancabello@gmail.com>
2018-02-08 10:18:35 +0100

    Añado ejercicio de interfaces y polimorfismo (más teoría).

---
+++ Proyectos/App_POO_Interface/App_POO_Interface/IPrueba.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_POO_Interface
+{
+    public delegate void DAnotacion(string s);
+
+
+    interface IPrueba
+    {
+        void Mostrar();
+        string Anotacion { get; set; }
+        string this[int indice] { get; }
+        event DAnotacion Anadido;
+    }
+
+
+    class Agenda : IPrueba
+    {
+        public void Mostrar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Anotacion
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string this[int indice]
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public event DAnotacion Anadido;
+    }
+
+}
---
+++ Proyectos/App_POO_Interface/App_POO_Interface/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_POO_Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factura f = new Factura();
+            Pila p = new Pila();
+
+            Copiar(f);
+            Copiar(p);
+
+
+            Console.ReadKey();
+        }
+
+        static void Copiar(ICloneable objeto)
+        {
+            //if(objeto is Factura) El operador is debuelve un bool, true si es del mismo tipo, false en caso contrario.
+
+            Console.WriteLine(objeto.Clone());
+        }
+    }
+
+    class Factura:ICloneable
+    {
+        public object Clone()
+        {
+            Console.WriteLine("Soy Factura.Clonar()");
+            return null;
+        }
+    }
+
+    class Pila:ICloneable
+    {
+        public object Clone()
+        {
+            Console.WriteLine("Soy Pila.Clonar()");
+            return null;
+        }
+    }
+}
