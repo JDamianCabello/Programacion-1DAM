a05ba23d14e42bbc164418c72e446f311ffbb5e8
Damián Cabello <jdamiancabello@gmail.com>
2018-02-16 01:22:46 +0100

    Agregación de nuevas opciones.

--- Proy. Propios/Matrix/Matrix/Program.cs
+++ Proy. Propios/Matrix/Matrix/Program.cs
@@ -4,12 +4,19 @@ namespace Matrix
 {
     class Program
     {
+        static int anchoTablero = Console.WindowWidth - 1;
+        static int altoTablero = Console.WindowHeight;
+        static int MAXANCHOTABLERO = Console.WindowWidth - 1;
+        static int MAXALTOTABLERO = Console.WindowHeight;
         static int velocidad = 30;
-        static byte[,] tablero = TableroAlea(Console.WindowHeight, Console.WindowWidth-1);
+        static byte[,] tablero = TableroAlea(altoTablero, anchoTablero);
         static ConsoleColor colorFondo = ConsoleColor.DarkGray;
+        static ConsoleColor colorDefectoLetra1 = ConsoleColor.DarkCyan;
+        static ConsoleColor colorDefectoLetra2 = ConsoleColor.Gray;
         static bool resetAutomatico = false;
         static int contadorIteracionesReset = 5000;
         static int totalIteraciones = 0;
+
 
         static void Main(string[] args)
         {
@@ -17,13 +24,13 @@ namespace Matrix
 
             Console.CursorVisible = false;
             ConsoleKeyInfo tecla;
-            Console.Title = GeneraTitulo();
-
+            GeneraTitulo();
             PintaTableroInicio(HazString(tablero));
             do
             {
                 do
                 {
+                    Console.SetCursorPosition(0, 0);
                     if (resetAutomatico && totalIteraciones >= contadorIteracionesReset)
                     {
                         Console.Clear();
@@ -36,9 +43,23 @@ namespace Matrix
 
                 tecla = Console.ReadKey(true);
 
+                if (tecla.Key == ConsoleKey.C)
+                {
+                    Console.Clear();
+                    PintaTableroInicio(HazString(tablero));
+                }
+
+                if (tecla.Key == ConsoleKey.R)
+                {
+                    tablero = TableroAlea(altoTablero, anchoTablero);
+                    Console.Clear();
+                    PintaTableroInicio(HazString(tablero));
+                }
+
                 if (tecla.Key == ConsoleKey.Tab)
                 {
                     Opciones();
+                    tablero = TableroAlea(altoTablero, anchoTablero);
                     Console.Clear();
                     PintaTableroInicio(HazString(tablero));
                 }
@@ -46,29 +67,40 @@ namespace Matrix
             } while (tecla.Key != ConsoleKey.Escape);
         }
 
-        private static string GeneraTitulo()
+        /// <summary>
+        /// Genera el título del programa
+        /// </summary>
+        /// <returns></returns>
+        private static void GeneraTitulo()
         {
+            string tmp = string.Empty;
             if (resetAutomatico)
-                return "Reseteo automatico: Activado en " + contadorIteracionesReset + " iteraciones    Total Iteraciones: [" + totalIteraciones + "]    Opciones [Tab]      Creado por Damián Cabello"; 
+                tmp = "Reseteo automatico: Activado en " + contadorIteracionesReset + " iteraciones    Total iteraciones: [" + totalIteraciones + "]    Opciones [Tab]    Restablece de inicio [C]    Reinica todo [R]      Creado por Damián Cabello"; 
             else
-                return "Reseteo automatico: Desactivado    Total Iteraciones: [" + totalIteraciones + "]    Opciones [Tab]    Creado por Damián Cabello";
+                tmp = "Reseteo automatico: Desactivado    Total iteraciones: [" + totalIteraciones + "]    Opciones [Tab]    Restablece de inicio [C]    Reinica todo [R]      Creado por Damián Cabello";
+
+            Console.Title = tmp;
         }
 
+        /// <summary>
+        /// Controla las opciones del juego.
+        /// </summary>
         private static void Opciones()
         {
             Random rnd = new Random();
             totalIteraciones = 0;
             Console.Title = "Opciones";
             Console.ResetColor();
-            int separacion = 60;
+            const int SEPARACION = 60;
             ConsoleKeyInfo tecla;
             Console.CursorVisible = false;
             int opcionSeleccionada = 1;
-            ConsoleColor colorSeleccion = ConsoleColor.Cyan;
+            ConsoleColor colorSeleccion = ConsoleColor.Blue;
             string aux = string.Empty;
-
+            double tiempoPorLetra = 0;
             do
             {
+                tiempoPorLetra = velocidad * 0.001;
                 aux = resetAutomatico ? "   Activado" : "   Desactivado";
                 Console.Clear();
                 Console.ResetColor();
@@ -78,23 +110,24 @@ namespace Matrix
                 Console.WriteLine("\t\t\tPulse la tecla [Esc] Escape, para volver a Matrix.");
                 Console.WriteLine("\n");
                 Console.WriteLine("Nota  : El total de iteraciones se refiere al total de letras que ha cambiado de color");
-                Console.WriteLine("Nota 2: El reset automático hace que el juego se restablezca automáticamente (en N iteraciones seleccionadas debajo).\n");
+                Console.WriteLine("Nota 2: El reset automático hace que el juego se restablezca automáticamente (en N iteraciones seleccionadas debajo).");
+                Console.WriteLine("Nota 3: El color del efecto cascada nunca será el mismo que el color que haya seleccionado de fondo.\n");
                 Console.ForegroundColor = (ConsoleColor)rnd.Next(1, 16);
-                Console.WriteLine("Mis repositorios de Git: https://github.com/JDamianCabello".PadLeft(separacion + 30));
+                Console.WriteLine("Mis repositorios de Git: https://github.com/JDamianCabello".PadLeft(SEPARACION + 30));
                 Console.ResetColor();
                 Console.SetCursorPosition(0, 0);
                 Console.CursorTop = 3;
-                Console.WriteLine("                OPCIONES".PadLeft(separacion));
-                Console.WriteLine("====================================================".PadLeft(separacion + 20));
+                Console.WriteLine("                OPCIONES".PadLeft(SEPARACION));
+                Console.WriteLine("====================================================".PadLeft(SEPARACION + 20));
                 Console.WriteLine();
                 if (opcionSeleccionada == 1)
                     Console.ForegroundColor = colorSeleccion;
-                Console.Write("Velocidad:".PadLeft(separacion));
-                Console.WriteLine("   {0}", velocidad);
+                Console.Write("Velocidad:".PadLeft(SEPARACION));
+                Console.WriteLine("   {0}\tEsquivale a: "+tiempoPorLetra+" segundo/s por letra", velocidad);
                 Console.ResetColor();
                 if (opcionSeleccionada == 2)
                     Console.ForegroundColor = colorSeleccion;
-                Console.Write("Reset Automático:".PadLeft(separacion));
+                Console.Write("Reset Automático:".PadLeft(SEPARACION));
                 Console.WriteLine("{0}",aux);
                 Console.ResetColor();
                 if (!resetAutomatico)
@@ -103,15 +136,45 @@ namespace Matrix
                 }
                 if(opcionSeleccionada == 3 && resetAutomatico)
                     Console.ForegroundColor = colorSeleccion;
-                Console.Write("Total iteraciones antes del reset:".PadLeft(separacion));
+                Console.Write("Total iteraciones antes del reset:".PadLeft(SEPARACION));
                 Console.WriteLine("   {0}", contadorIteracionesReset);
                 Console.ResetColor();
-                Console.WriteLine("\n\n");
                 if (opcionSeleccionada == 4)
                     Console.ForegroundColor = colorSeleccion;
-                Console.Write("Seleccionar color de fondo:".PadLeft(separacion));
+                Console.Write("Largo del recuadro:".PadLeft(SEPARACION));
+                Console.WriteLine("   {0}", anchoTablero);
+                Console.ResetColor();
+                if (opcionSeleccionada == 5)
+                    Console.ForegroundColor = colorSeleccion;
+                Console.Write("Alto del recuadro:".PadLeft(SEPARACION));
+                Console.WriteLine("   {0}", altoTablero);
+                Console.ResetColor();
+                Console.WriteLine("\n\n");
+                if (opcionSeleccionada == 6)
+                    Console.ForegroundColor = colorSeleccion;
+                Console.Write("Seleccionar color de fondo:".PadLeft(SEPARACION));
                 Console.Write(" |");
                 Console.BackgroundColor = colorFondo;
+                Console.Write("   ");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.Write("|");
+                Console.ResetColor();
+                Console.WriteLine();
+                if (opcionSeleccionada == 7)
+                    Console.ForegroundColor = colorSeleccion;
+                Console.Write("Selecciona el color por defecto de letra 1:".PadLeft(SEPARACION));
+                Console.Write(" |");
+                Console.BackgroundColor = colorDefectoLetra1;
+                Console.Write("   ");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.Write("|");
+                Console.ResetColor();
+                Console.WriteLine();
+                if (opcionSeleccionada == 8)
+                    Console.ForegroundColor = colorSeleccion;
+                Console.Write("Selecciona el color por defecto de letra 2:".PadLeft(SEPARACION));
+                Console.Write(" |");
+                Console.BackgroundColor = colorDefectoLetra2;
                 Console.Write("   ");
                 Console.BackgroundColor = ConsoleColor.Black;
                 Console.Write("|");
@@ -120,7 +183,7 @@ namespace Matrix
 
                 if (tecla.Key == ConsoleKey.DownArrow)
                 {
-                    if (opcionSeleccionada != 4)
+                    if (opcionSeleccionada != 8)
                         if(opcionSeleccionada == 2 && !resetAutomatico)
                             opcionSeleccionada += 2;
                         else
@@ -155,8 +218,24 @@ namespace Matrix
                                 contadorIteracionesReset -= 500;
                             break;
                         case 4:
+                            if (anchoTablero != 5)
+                                anchoTablero -= 1;
+                            break;
+                        case 5:
+                            if (altoTablero != 5)
+                                altoTablero -= 1;
+                            break;
+                        case 6:
                             if (colorFondo != ConsoleColor.Black)
-                                colorFondo = (ConsoleColor)colorFondo - 1;
+                                colorFondo = colorFondo - 1;
+                            break;
+                        case 7:
+                            if (colorDefectoLetra1 != ConsoleColor.Black)
+                                colorDefectoLetra1 -= 1;
+                            break;
+                        case 8:
+                            if (colorDefectoLetra2 != ConsoleColor.Black)
+                                colorDefectoLetra2 -= 1;
                             break;
                     }
                 }
@@ -180,8 +259,24 @@ namespace Matrix
                                 contadorIteracionesReset += 500;
                             break;
                         case 4:
+                            if (anchoTablero != MAXANCHOTABLERO)
+                                anchoTablero += 1;
+                            break;
+                        case 5:
+                            if (altoTablero != MAXALTOTABLERO)
+                                altoTablero += 1;
+                            break;
+                        case 6:
                             if (colorFondo != ConsoleColor.White)
-                                colorFondo = (ConsoleColor)colorFondo + 1;
+                                colorFondo = colorFondo + 1;
+                            break;
+                        case 7:
+                            if (colorDefectoLetra1 != ConsoleColor.White)
+                                colorDefectoLetra1 = colorDefectoLetra1 + 1;
+                            break;
+                        case 8:
+                            if (colorDefectoLetra2 != ConsoleColor.White)
+                                colorDefectoLetra2 = colorDefectoLetra2 + 1;
                             break;
                     }
                 }
@@ -193,6 +288,7 @@ namespace Matrix
 
         private static byte[,] TableroAlea(int alto, int largo)
         {
+
             Random rnd = new Random();
             byte[,] aux = new byte[alto, largo];
             for(int i = 0; i < alto; i++)
@@ -232,10 +328,10 @@ namespace Matrix
                 {
 
                     case 0:
-                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Console.ForegroundColor = colorDefectoLetra1;
                         break;
                     case 1:
-                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.ForegroundColor = colorDefectoLetra2;
                         break;
                 }
                 Console.Write(tablero[i]);
@@ -253,44 +349,17 @@ namespace Matrix
             for(int i = 0; i <= posDesplazamiento; i++)
             {
                 Console.SetCursorPosition(ejeX, ejeY + i);
-                
-                Console.ForegroundColor = (ConsoleColor)(rnd.Next(0, 16));
+
+                do
+                {
+                    Console.ForegroundColor = (ConsoleColor)(rnd.Next(0, 16)); 
+                } while (Console.ForegroundColor == colorFondo);
                 Console.Write(a[ejeY,ejeX]);
                 System.Threading.Thread.Sleep(velocidad);
                 totalIteraciones++;
-                Console.Title = GeneraTitulo();
+                GeneraTitulo();
             }
 
         }
-
-        private static void PintaInicio(byte[,] a)
-        {
-            Random rnd = new Random();
-
-            for(int i = 0; i < a.GetLength(0); i++)
-            {
-                for(int j = 0; j < a.GetLength(1); j++)
-                {
-                    switch(rnd.Next(0,2))
-                    {
-                        
-                        case 0:
-                            Console.ForegroundColor = ConsoleColor.DarkCyan;
-                            break;
-                        case 1:
-                            Console.ForegroundColor = ConsoleColor.DarkBlue;
-                            break;
-                    }
-
-                    Console.Write(a[i, j]);
-                    Console.ResetColor();
-                    System.Threading.Thread.Sleep(1);
-                }
-                Console.WriteLine();
-            }
-        }
-
-
-
     }
 }
