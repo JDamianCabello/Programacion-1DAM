7e6e685fa66f89044141062505ed0a63fd252bb8
Damián Cabello <jdamiancabello@gmail.com>
2018-02-22 08:39:06 +0100

    Añado un ejemplo de la clase Directory.

---
+++ Proyectos/App_Directory/App_Directory/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace App_Directory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ruta = @"..\..\..\..\";
+            string[] ficheros = Directory.GetDirectories(ruta);
+
+            foreach(var item in ficheros)
+                Console.WriteLine(item.Substring(12));
+
+            Console.ReadKey();
+        }
+    }
+}
