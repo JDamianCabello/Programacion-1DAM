d956021b6e843ad56dde6336ec401fc72ab62c76
Damián Cabello <jdamiancabello@gmail.com>
2018-03-05 11:47:30 +0100

    Ejercicios 5 y 6 terminados.

---
+++ T8/Ejer5/Ejer5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ejer5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0 || args.Length > 1)
+                Console.WriteLine("No se especificó la ruta correctamente (cero o más de un argumentos).");
+            else if(!File.Exists(args[0]))
+                Console.WriteLine("No existe el archivo!");
+            else
+            {
+                string[] texto = File.ReadAllLines(args[0],Encoding.Default);
+                foreach(var item in texto)
+                    Console.WriteLine(item);
+            }
+        }
+
+
+
+    }
+}
---
+++ T8/Ejer6/Ejer6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ejer6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length != 2)
+                Console.WriteLine("Error de argumentos (cero o más de dos)");
+            else if(!File.Exists(args[1]))
+                Console.WriteLine("El archivo no existe!");
+            else
+            {
+                int contador = 0;
+                string texto = File.ReadAllText(args[1],Encoding.Default);
+
+                foreach(var item in texto)
+                    if(args[0][0] == item)
+                        contador++;
+
+
+                Console.WriteLine("El caracter \"{0}\" aparece {1} vez/veces",args[0],contador);
+            }
+        }
+    }
+}
