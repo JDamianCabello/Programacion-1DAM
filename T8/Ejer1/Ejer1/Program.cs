cf5994ae5d32632b1d0e94e20335258be60dbf54
Damián Cabello <jdamiancabello@gmail.com>
2018-03-05 09:04:11 +0100

    Refactorización de ficheros de cara al examen, se añaden ejercicios del tema 8 también.

---
+++ T8/Ejer1/Ejer1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Ejer1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ruta = @"../../../../_txtRelacion/txtEjer1.txt";
+            Console.CursorVisible = false;
+            if(!File.Exists(ruta))
+                File.Create(ruta);
+
+            Console.WriteLine("     Datos Obtenidos con la clase Path");
+            Console.WriteLine("     =================================");
+            Console.WriteLine("                   Raíz: {0}", Path.GetPathRoot(Path.GetFullPath(ruta)));
+            Console.WriteLine("          Ruta Absoluta: {0}",Path.GetFullPath(ruta));
+            Console.WriteLine("     Nombre del fichero: {0}",Path.GetFileName(ruta));
+            Console.WriteLine("  Extensión del fichero: {0}",Path.GetExtension(ruta));
+
+
+            Console.WriteLine("\n\n\tPulsa una tecla para salir.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ T8/Ejer2/Ejer2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Ejer2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            DirectoryInfo carpeta = new DirectoryInfo(@"../../../../_txtRelacion/");
+
+            if(!carpeta.Exists)
+                carpeta.Create();
+
+            Console.WriteLine("     Datos Obtenidos con la clase Path");
+            Console.WriteLine("     =================================");
+            Console.WriteLine("                    Ruta: {0}", carpeta.FullName);
+            Console.WriteLine("Atributos del directorio: {0}", carpeta.Attributes);
+            Console.WriteLine("   Nombre del directorio: {0}",carpeta.Name);
+            Console.WriteLine("\n\n\tFicheros en el directorio:");
+
+            FileInfo[] ficheros = carpeta.GetFiles();
+            for(int i = 0; i < ficheros.Length; i++)
+                Console.WriteLine("\t{{{0,2}}}\t{1}",i+1,ficheros[i].Name);
+
+
+            Console.WriteLine("\n\n\tPulsa una tecla para salir.");
+            Console.ReadKey();
+        }
+    }
+}
