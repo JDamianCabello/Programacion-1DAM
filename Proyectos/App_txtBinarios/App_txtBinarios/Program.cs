8daccece4c0e97ae41202bc50569e837d776bf68
Damián Cabello <jdamiancabello@gmail.com>
2018-03-08 09:55:39 +0100

    Añado ejemplo de serialización de objetos.

---
+++ Proyectos/App_txtBinarios/App_txtBinarios/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace App_txtBinarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Escribe();
+            Lee();
+            Console.ReadKey();
+        }
+
+        static void Lee()
+        {
+            string ruta = @"c:/basura/txtBinario.txt";
+
+            FileStream fs = new FileStream(ruta, FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+
+            Console.WriteLine(br.ReadString());
+            Console.WriteLine(br.ReadInt32());
+            Console.WriteLine(br.ReadDouble());
+
+            fs.Dispose();
+        }
+
+        static void Escribe()
+        {
+            string ruta = @"c:/basura/txtBinario.txt";
+            string texto = "Tengo sueño!";
+            int entero = 14;
+            double doble = Math.PI;
+
+
+            FileStream fs = new FileStream(ruta, FileMode.OpenOrCreate, FileAccess.Write);
+
+            BinaryWriter bw = new BinaryWriter(fs);
+
+            bw.Write(texto);
+            bw.Write(entero);
+            bw.Write(doble);
+
+            fs.Close();
+
+            if(bw != null)
+                bw.Close();
+        }
+    }
+}
