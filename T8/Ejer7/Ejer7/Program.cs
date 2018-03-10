29eba72da73f5cc8b29996b1477ca1d553935fef
Damián Cabello <jdamiancabello@gmail.com>
2018-03-10 18:00:33 +0100

    Termino el ejercicio 7.

---
+++ T8/Ejer7/Ejer7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejer7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length != 2)
+                Console.WriteLine("Este programa necesita dos parámetros!");
+            else
+            {
+                if(!File.Exists(args[0]) || !File.Exists(args[1]))
+                    Console.WriteLine("Alguna de las rutas no existe.");
+                else
+                {
+                    File.AppendAllText(args[0], " " +File.ReadAllText(args[1]));
+                }
+            }
+        }
+    }
+}
