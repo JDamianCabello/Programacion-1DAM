197bc792b02ede24430e5ca9b4d8f6afec0b17fa
Damián Cabello <jdamiancabello@gmail.com>
2018-03-11 19:39:57 +0100

    Añado ejercicio 11 terminado.

---
+++ T8/Ejer11/Ejer11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ejer11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto = string.Empty;
+            string todo = string.Empty;
+            string ruta = @"E:\Google Drive\Git\Programacion\T8\_txtRelacion\txtEjer11.txt";
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Para salir escribe en blanco");
+                Console.WriteLine("Escribe lo que quieras guardar en el fichero: ");
+                texto = Console.ReadLine();
+                if (texto != "")
+                    todo += texto + "\n";
+            } while (texto != "");
+            File.AppendAllText(ruta, todo);
+        }
+    }
+}
