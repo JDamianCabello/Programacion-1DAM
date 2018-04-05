974c99de6fba673e820f7d94c809971603b064a3
Damián Cabello <jdamiancabello@gmail.com>
2018-04-05 09:36:34 +0200

    Añado ejemplos de selectores y teoría.

---
+++ Proyectos/WPF_Canvas/WPF_Canvas/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Canvas
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Rectangle rectangulo = new Rectangle();
+            rectangulo.Width = 50;
+            rectangulo.Height = 50;
+            rectangulo.Fill = new SolidColorBrush(Colors.Blue);
+
+            //Añadirlo al canvas
+            Canvas.SetLeft(rectangulo, 20);
+            Canvas.SetTop(rectangulo, 200);
+
+            cnv_panel.Children.Add(rectangulo);
+        }
+
+        void 
+    }
+}
---
+++ Proyectos/WPF_selectores/WPF_selectores/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//----------------------------
+using System.ComponentModel;
+
+namespace WPF_selectores
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_ordenar_Click(object sender, RoutedEventArgs e)
+        {
+            lbx_lista.Items.SortDescriptions.Add(new SortDescription("Content", ListSortDirection.Ascending));
+        }
+
+        private void cbx_Ordenar_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            lbx_lista.Items.SortDescriptions.Clear();
+
+            if(cbx_Ordenar.SelectedIndex == 0)
+            {
+                lbx_lista.Items.SortDescriptions.Add(new SortDescription("Content", ListSortDirection.Ascending));
+                lbl_tipoOrdenacion.Content = "Ordenación: Ascendente";
+            }
+            else if(cbx_Ordenar.SelectedIndex == 1)
+            {
+                lbx_lista.Items.SortDescriptions.Add(new SortDescription("Content", ListSortDirection.Descending));
+                lbl_tipoOrdenacion.Content = "Ordenación: Descendente";
+            }
+        }
+    }
+}
